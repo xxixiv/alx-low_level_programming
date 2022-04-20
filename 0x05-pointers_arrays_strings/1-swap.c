7875539385ee88606986b26f4529c7ffb1beb64b
MercX <freddiecharlesjnr@gmail.com>
2022-04-20 17:48:50 +0100

    i'm pointing at arrays and strings c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,6 @@
 #include "main.h"
- /**
+
+/**
   * swap_int - swap variable values
   * @a: pointer 1
   * @b: pointer 2
@@ -7,8 +8,9 @@
   */
 
 void swap_int(int *a, int *b)
- {
+
+{
          *a += *b;
          *b = *a - *b;
          *a = *a - *b;
- }
+}
